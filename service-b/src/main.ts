234aad6fd5e90340224337250a067be49b4d8d73
Samuel Ramon <samuelrbo@gmail.com>
2022-06-18 08:38:48 +0100

    feat(service-b): Simple example of a micro-service

---
+++ service-b/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        host: '127.0.0.1',
+        port: 8889,
+      },
+    },
+  );
+
+  app.listen();
+}
+bootstrap();
